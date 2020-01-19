ff5b651b2dc2661982e3968243e001f39b84576d
Michael k <michaelkahnt123@gmail.com>
2020-01-19 19:37:04 +0100

    Update 2.7 konstanten 1.cpp

--- 2.7 konstanten 1.cpp
+++ 2.7 konstanten 1.cpp
@@ -4,23 +4,29 @@ using namespace std;
 
 int main()
 {
-	
-	enum raumschifftyp {jaeger, transporter, minenleger, bomber}; /* eine möglichkeit eine konstante zu erzeugen.
+	// Es gibt 3 Möglichkeiten Konstanten zu erzeugen:
+
+	// 1:
+
+	enum raumschifftyp { jaeger, transporter, minenleger, bomber }; /* eine möglichkeit eine konstante zu erzeugen.
 	mit dem befehl "enum". Hierbei ist jäger = 0, transporter = 1 usw.*/
 	raumschifftyp spieler1 = transporter;
-	
+
 	/* man könnte auch beispielsweise "transporter = 3" schreiben dann wäre transporter = 3, minenleger = 4 usw.*/
-	
+
+	// 2:
+
 	const int ianzahl_level = 8;  /* "const" für konstenten.
 	damit legt man einmal einen wert fest und kann diesen später nicht mehr ändern.
 	wird benutzt damit man einen wichtigen wert, wie z.b.:
 	die anzahl der level nicht später irgendwo versehentlich ändert.*/
-	
-	cout << ianzahl_level << endl;
-	
 
-return 0;
-	
-} /* es gibt noch eine dritte möglichkeit eine konstante zu erzeugen mit
-dem präprozessor befehl "#define anzahl_level 10", diese ist aber weniger geeignet
-als die anderen beiden, siehe buch 2.7.*/
+	cout << ianzahl_level << endl;
+
+	// 3:
+
+#define anzahl_lvl 7 // Bei define wird kein Semikolon am ende benutzt, da es eine präprozessor Direktive ist.
+
+	return 0;
+
+} 
