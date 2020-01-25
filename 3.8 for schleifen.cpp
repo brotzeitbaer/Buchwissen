dcdc420fde6debba06befcb008e755c13012272e
Michael k <michaelkahnt123@gmail.com>
2020-01-25 16:19:31 +0100

    Update 3.8 for schleifen.cpp

--- 3.8 for schleifen.cpp
+++ 3.8 for schleifen.cpp
@@ -5,18 +5,22 @@ using namespace std;
 int main()
 {
 	
-	int rundenpunkte, rundenanzahl, gesamtpunkte;
+	int rundenpunkte = 0;
+	int rundenzahl = 0;
+	int gesamtpunkte = 0;
 	
 	cout << "wieviel runden sollen gespielt werden?`\n";
 	cin  >> rundenanzahl;
 	
+	
+	// Der Aktionsteil (i++) wird erst ausgeführt nachdem der Codeblock einmal ausgeführt wurde.
 	for (int i = 0; i < rundenanzahl; i++)
 	
 	/*in den klammern der for schleife stehen sozusagen die bedingungen,
 	diese bestehen aus drei teilen. erstens der initialisierungsteil, danach der bedingungsteil
 	und als letztes der aktionsteil.*/
 	
-	{
+	{ 
 	cout << "runde " << i + 1 << " laeuft\n";
 	cout << "erreichte punkte in dieser runde eingeben: \n";
 	cin  >> rundenpunkte;
