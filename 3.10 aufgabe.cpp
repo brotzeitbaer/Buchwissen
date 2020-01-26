65f4f4bd29472daa553c6b8c37e5dc8e25ed61c9
Michael k <michaelkahnt123@gmail.com>
2020-01-26 17:43:55 +0100

    Update 3.10 aufgabe.cpp

--- 3.10 aufgabe.cpp
+++ 3.10 aufgabe.cpp
@@ -8,49 +8,37 @@ int main()
 	int zahl = 0;
 	int haelfte = 0;
 
-	cout << "Zahl eingeben um Countdown zu starten: " << endl;
+	cout << "Zahl eingeben: " << endl;
 	cin >> zahl;
 
-	haelfte = zahl / 2;
+	haelfte = zahl/2;
 
-	if (zahl % 2 == 0)
+	if (zahl%2 == 0)
 	{
-		for (; zahl > 0;)
-		{
-
+		for (; zahl>0;)
+		{ 
 			if (zahl == haelfte)
 			{
-				cout << "Haelfte erreicht. " << "(" << zahl << ")" << endl;
-
+				cout << "Haelfte erreicht " << "(" << zahl << ")" << endl;
 				zahl--;
-
 			}
-
-			zahl--;
-
-			cout << "Countdown: " << zahl+1 << endl;
-
+		zahl--;
+		cout << "Countdown: " << zahl+1 << endl;
 		}
-	
 	}
 	else
 	{
-		for (; zahl > 0;)
-		{
-
+		for (;zahl>0;)
+		{ 
 			if (zahl == haelfte)
 			{
-				cout << "Haelfte erreicht. " << "(" << zahl+0.5 << ")" << endl;
-
+				cout << "Haelfte erreicht" << "(" << zahl+0.5 << ")" << endl;
 				zahl--;
 			}
-
-			zahl--;
-
-			cout << "Countdown: " << zahl+1 << endl;
+		zahl--;
+		cout << "Countdown: " << zahl+1 << endl;
 		}
 	}
 
 	return 0;
-
 }
